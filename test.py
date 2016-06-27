ee9289a51ef3fcd4107db4a75b6132ba865514b0
bogdan <koboldul@gmail.com>
2016-06-27 16:37:18 -0300

    merge

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import smtplib
+import RPi.GPIO as GPIO
+from  email.mime.text import MIMEText
+
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(17, GPIO.IN)
+#print GPIO.input(17)
 
 smtpObj = smtplib.SMTP
 
@@ -8,20 +14,17 @@ to = 'koboldul@gmail.com'
 url = 'smtp.sendgrid.net'
 port = int(587)
 
-message = """\
-        From: %s
-        To: %s
-        Subject: %s
-        %s
-        """ % (acc, ", ".join(to), 'sbj', 'mergeee')
-# me == the sender's email address
-# you == the recipient's email address
+message = MIMEText('mmmmmmm')
+message['Subject']='weather bot'
+message['From']='farmbot@sendgrid.com'
+message['To']='koboldul@gmail.com'
+
 
 try:
 	smtpObj = smtplib.SMTP(url, port)
 	smtpObj.starttls()
 	smtpObj.login('koboldul', passW)
-	smtpObj.sendmail(acc, to, message)         
+	smtpObj.sendmail(acc, to, message.as_string())         
 	print "Successfully sent email"
 except Exception as e:
 	print e
